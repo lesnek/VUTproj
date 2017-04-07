f5683a0d75986a1e035a38dbf1a941e0e92dc538
Lesnek <lesnek@eurosat.cz>
2017-04-07 11:40:00 +0200

    :( sessiona

--- postava.php
+++ postava.php
@@ -5,6 +5,19 @@
  * Date: 7.4.17
  * Time: 9:20
  */
+session_start();
+require_once 'class.user.php';
+$user_home = new USER();
+
+if(!$user_home->is_logged_in())
+{
+    $user_home->redirect('index.php');
+}
+
+$stmt = $user_home->runQuery("SELECT * FROM tbl_users WHERE userID=:uid");
+$stmt->execute(array(":uid"=>$_SESSION['userSession']));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
