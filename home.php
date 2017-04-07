de7f1f65f3d9bac949c6cb532506cdff585c3224
Lesnek <lesnek@eurosat.cz>
2017-04-07 11:06:09 +0200

    changes

--- home.php
+++ home.php
@@ -29,63 +29,10 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
         <link href="assets/styles.css" rel="stylesheet" media="screen">
-        
+        <?php include_once "navMenu.php"?>
     </head>
     
     <body>
-        <div class="navbar navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container-fluid">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                    </a>
-                    <a class="brand" href="home.php">VUT game</a>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav pull-right">
-                            <li class="dropdown">
-                                <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"><?php echo $row['levl']; ?>  <i class="icon-user"></i>
-								<?php echo $row['userName']; ?> <i class="caret"></i>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a tabindex="-1" href="logout.php">Odhlásit</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <ul class="nav">
-                            <li class="active">
-                                <a href="http://www.codingcage.com/">Postava</a>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" data-toggle="dropdown" class="dropdown-toggle">Tutorials <b class="caret"></b>
-
-                                </a>
-                                <ul class="dropdown-menu" id="menu1">
-                                    <li><a href="http://www.codingcage.com/search/label/PHP OOP">PHP OOP</a></li>
-                                    <li><a href="http://www.codingcage.com/search/label/PDO">PHP PDO</a></li>
-                                    <li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
-                                    <li><a href="http://www.codingcage.com/search/label/Bootstrap">Bootstrap</a></li>
-                                    <li><a href="http://www.codingcage.com/search/label/CRUD">CRUD</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="http://www.codingcage.com/2015/09/login-registration-email-verification-forgot-password-php.html">Tutorial Link</a>
-                            </li>
-                            
-                            
-                        </ul>
-                    </div>
-                    <!--/.nav-collapse -->
-                </div>
-            </div>
-        </div>
-        
-        <!--/.fluid-container-->
-        <script src="bootstrap/js/jquery-1.9.1.min.js"></script>
-        <script src="bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/scripts.js"></script>
         
     </body>
 
---
+++ navMenu.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lesnek
+ * Date: 7.4.17
+ * Time: 11:00
+ */
+?>
+<body>
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container-fluid">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="home.php">VUT game</a>
+            <div class="nav-collapse collapse">
+                <ul class="nav pull-right">
+                    <li class="dropdown">
+                        <a href="#" role="button" class="dropdown-toggle"
+                           data-toggle="dropdown">lvl <?php echo $row['levl']; ?> <i class="icon-user"></i>
+                            <?php echo $row['userName']; ?> <i class="caret"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a tabindex="-1" href="logout.php">Odhlásit</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav">
+                    <li>
+                        <a href="postava.php">Postava</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle">Tutorials <b class="caret"></b>
+
+                        </a>
+                        <ul class="dropdown-menu" id="menu1">
+                            <li><a href="http://www.codingcage.com/search/label/PHP OOP">PHP OOP</a></li>
+                            <li><a href="http://www.codingcage.com/search/label/PDO">PHP PDO</a></li>
+                            <li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
+                            <li><a href="http://www.codingcage.com/search/label/Bootstrap">Bootstrap</a></li>
+                            <li><a href="http://www.codingcage.com/search/label/CRUD">CRUD</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="http://www.codingcage.com/2015/09/login-registration-email-verification-forgot-password-php.html">Tutorial
+                            Link</a>
+                    </li>
+
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="bootstrap/js/jquery-1.9.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/scripts.js"></script>
+</body>
